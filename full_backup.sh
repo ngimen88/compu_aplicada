3b311ff43228679aa356852bc05d592b87d94d3e
obskur123 <tomasxd768@outlook.com>
2020-10-26 01:00:45 -0300

    mofique el script de backup solo falta enviar el log al root y estamos con este punto

--- full_backup.sh
+++ full_backup.sh
@@ -5,23 +5,59 @@ CHECK_FILESYS1=false
 CHECK_FILESYS2=false
 
 
+TO_SAVE=''
+TO_BCKP=''
+
+CHECK_TOSAVE=false
+CHECK_TOBCKP=false
+
+NAME=''
+CHECK_NAME=false
+
+DATE=$(date +%Y-%m-%d)
+
+
+help_menu(){
+
+	echo "menu de ayuda";
+}
+
+
 check_filesystems(){
 
-	local DIR1=$1
-	local DIR2=$2
 
-	if mountpoint -q "$DIR1" && mountpoint -q "$DIR2";
+	if mountpoint -q "$1" && mountpoint -q "$2";
 	then
-		echo "$DIR1 y $DIR2 montados correctamente";
+		return 0
+		
 	else
-		echo "error";
+		return 1
+	
 	fi
 
 }
 
 
 
-while getopts 'a:b:' option
+do_backup(){
+
+	local DIR_BACKUP=$1
+	local DIR_SOURCE=$2
+	local FILESYS_SOURCE=$3
+	local FILESYS_BACKUP=$4
+	local NAME=$5
+	
+	
+	if check_filesystems "$FILESYS_SOURCE" "$FILESYS_BACKUP";
+	then
+		tar -cvzpf $DIR_BACKUP/$NAME-backup_$DATE.tar.gz $DIR_SOURCE;
+	fi
+
+
+}
+
+
+while getopts 'a:b:c:d:n:h' option
 do
 	case "${option}"
 	in
@@ -30,15 +66,25 @@ do
 
 		b) FILESYS2="${OPTARG}"
 		   CHECK_FILESYS2=true;;
+		
+		c) TO_SAVE="${OPTARG}"
+		   CHECK_TOSAVE=true;;
+
+		d) TO_BCKP="${OPTARG}"
+		   CHECK_TOBCKP=true;;
+		
+		n) NAME="${OPTARG}"
+		   CHECK_NAME=true;;
+
+		h) help_menu;;
 	esac
 done
 
 
-if [ "$CHECK_FILESYS1" = true ] && [ "$CHECK_FILESYS2" = true ]; then
-	check_filesystems "$FILESYS1" "$FILESYS2";
-else
-	echo "error en argumentos";
+if [ "$CHECK_TOSAVE" = true ] && [ "$CHECK_TOBCKP" = true ] && [ "$CHECK_FILESYS1" = true ] &&
+[ "$CHECK_FILESYS2" = true ] && [ "$CHECK_NAME" = true ]; 
+then
+	do_backup "$TO_SAVE" "$TO_BCKP" "$FILESYS1" "$FILESYS2" "$NAME";
+
 fi
-
-
 
